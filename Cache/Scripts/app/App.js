1f088242b32b32d2b3da0580f7fa3352d3c65358
Nicolas Buzzi <xxneecoxx@gmail.com>
2017-12-24 15:50:12 -0300

    Deleting unnecesary files

---
+++ Cache/Scripts/app/App.js
@@ -0,0 +1,25 @@
+/// <reference path="../typings/jquery/jquery.d.ts" />
+/// <reference path="../typings/typings.d.ts" />
+/// <reference path="../typings/es6-promise/es6-promise.d.ts" />
+/// <reference path="../typings/bootstrap/index.d.ts" />
+//Document ready
+$(document).ready(function () {
+    $('#data').DataTable({
+        "processing": true,
+        "serverSide": true,
+        "ajax": "/Home/Get",
+        "columns": [
+            { "title": "Event Name", "data": "EventName", "searchable": true },
+            { "title": "Start Date", "data": "StartDate", "searchable": true },
+            { "title": "End Date", "data": "EndDate", "searchable": true }
+        ],
+        "lengthMenu": [[10, 25, 50, 100], [10, 25, 50, 100]]
+    });
+});
+$(document).ajaxStart(function () {
+    //
+});
+$(document).ajaxStop(function () {
+    //
+});
+//# sourceMappingURL=App.js.map
